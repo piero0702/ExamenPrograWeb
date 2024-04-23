3f6730da4920561d2edf4358d8281855f0f5aaca
piero0702 <pieropizarrobasile@gmail.com>
2024-04-22 19:41:23 -0500

    Elementos ejercicio 2 examen

--- Ejercicio2/script.js
+++ Ejercicio2/script.js
@@ -1,5 +1,5 @@
 function calcularFarenheit(){
     const dato = documet.getElementById("elemento").value;
     let valorFarenheit = (parseInt(dato) - 32)*5/9;
-    document.getElementById("yop").innerHTML = valorFarenheit
+    alert(valorFarenheit);
 }
